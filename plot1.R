bce26dbe3c4f9e4f4dfa81b6e34a2542585e6d21
samot001 <t.infuehr@gmx.at>
2015-11-06 20:21:46 +0100

    upload solution

---
+++ plot1.R
@@ -0,0 +1,13 @@
+dataFile <- "./data/household_power_consumption.txt"
+data <- read.table(dataFile, header=TRUE, sep=";", stringsAsFactors=FALSE, dec=".")
+subSetData <- data[data$Date %in% c("1/2/2007","2/2/2007") ,]
+
+#str(subSetData)
+globalActivePower <- as.numeric(subSetData$Global_active_power)
+
+## Plot 1
+hist(globalActivePower, main="Global Active Power",xlab="Global Active Power (kilowatts)", ylab="Frequency", col="Red")
+
+## Saving to file
+dev.copy(png, file="plot1.png", height=480, width=480)
+dev.off()
